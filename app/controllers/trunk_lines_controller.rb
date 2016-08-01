76e6a7264197bc055f2d24a2b5a47111833b29a3
James <james1239090@gmail.com>
2016-08-01 21:59:33 +0800

    set the index controller to show a single marker

--- app/controllers/trunk_lines_controller.rb
+++ app/controllers/trunk_lines_controller.rb
@@ -1,2 +1,13 @@
 class TrunkLinesController < ApplicationController
+
+  def index
+    @trunklines = TrunkLine.first
+
+    @hash = Gmaps4rails.build_markers(@trunklines) do |trunkline, marker|
+      marker.lat trunkline.latitude
+      marker.lng trunkline.longitude
+      marker.infowindow trunkline.full_address
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'trunklines#index'
+  root 'trunk_lines#index'
   resources :trunk_lines
 end
