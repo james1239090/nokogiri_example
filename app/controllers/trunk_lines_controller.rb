f883230ccd280aaa0a8f40f575b99719423d0a7e
James <james1239090@gmail.com>
2016-08-04 23:10:39 +0800

    infobox with 2 values

--- app/controllers/trunk_lines_controller.rb
+++ app/controllers/trunk_lines_controller.rb
@@ -6,7 +6,7 @@ class TrunkLinesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@trunklines) do |trunkline, marker|
       marker.lat trunkline.latitude
       marker.lng trunkline.longitude
-      marker.infowindow "<div class='col-xs-12'>#{trunkline.full_address}</div>"
+      marker.infowindow "<div class='col-xs-12'><table><tbody><tr><td>#{trunkline.full_address}</td>#{trunkline.around_time}<td></td><tr></tbody><table></div>"
     end
   end
 
