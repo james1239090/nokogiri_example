f5f581c8e6fc6c403f5ebc24641f694cc8d61ff3
James <james1239090@gmail.com>
2016-08-05 23:42:02 +0800

    infobox with partial view

--- app/controllers/trunk_lines_controller.rb
+++ app/controllers/trunk_lines_controller.rb
@@ -6,7 +6,7 @@ class TrunkLinesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@trunklines) do |trunkline, marker|
       marker.lat trunkline.latitude
       marker.lng trunkline.longitude
-      marker.infowindow "<div class='col-xs-12'><table><tbody><tr><td>#{trunkline.full_address}</td>#{trunkline.around_time}<td></td><tr></tbody><table></div>"
+      marker.infowindow render_to_string(:partial=> 'info_box', :locals => { :trunkline => trunkline})
     end
   end
 
