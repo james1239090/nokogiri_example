7d115fa15b0a859e919c8dda1e0ed3e8ba342e2f
James <james1239090@gmail.com>
2016-08-07 17:06:02 +0800

    try the submit_tag with search box

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'trunk_lines#index'
-  resources :trunk_lines
+  resources :trunk_lines do
+    member do
+      get :search
+    end
+  end
 end
