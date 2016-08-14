0e5a74a12c6477aa61b9d0ffa0f275daa7a63ad0
James <james1239090@gmail.com>
2016-08-14 14:04:52 +0800

    add the checkbox for search by address in near place

--- app/controllers/trunk_lines_controller.rb
+++ app/controllers/trunk_lines_controller.rb
@@ -2,14 +2,17 @@ class TrunkLinesController < ApplicationController
 
   def index
     @search  = params[:search] || {}
-    @trunklines = TrunkLine.search(@search[:address])
+    if (@search[:range] == "1")
+      @trunklines = TrunkLine.near(@search[:address],0.5, units: "km")
+    else
+      @trunklines = TrunkLine.search(@search[:address])
+    end
 
     @hash = Gmaps4rails.build_markers(@trunklines) do |trunkline, marker|
       marker.lat trunkline.latitude
       marker.lng trunkline.longitude
       marker.infowindow render_to_string(:partial=> 'info_box', :locals => { :trunkline => trunkline})
     end
-
   end
 
 end
